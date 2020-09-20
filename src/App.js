cd800f11d0b524d3628b39c0b3accadb392186ab
YERIM <dnaer456@gmail.com>
2020-09-20 15:15:48 +0900

    btn

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
+import Button from "./Button";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
   render() {
-    return <div>Hi</div>;
+    return (
+      <div>
+        <Button />
+        <Button />
+      </div>
+    );
   }
 }
 export default App;
